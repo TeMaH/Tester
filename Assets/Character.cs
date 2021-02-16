4b0e4c190ebad1847956e9abf2c347a9af499229
Artem Mazgo <artsiom.mazgo@gmail.com>
2021-02-16 20:37:06 +0300

    Animator v1

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -4,36 +4,44 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
-    public float movementSpeed = 10.0f;
+    public float movementSpeed = 2.0f;
+    public float sprintSpeed = 5.0f;
     public float rotationSpeed = 0.2f;
+    public float animationBlendSpeed = 0.2f;
 
     CharacterController controller;
+    Animator animator;
     Camera characterCamera;
     float rotationAngle = 0.0f;
+    float targetAnimationSpeed = 0.0f;
+    bool isSprint = false;
 
-    public CharacterController Controller 
-    {
-        get { return controller = controller ?? GetComponent<CharacterController>(); } 
-    }
-    public Camera CharacterCamera 
-    { 
-        get { return characterCamera = characterCamera ?? FindObjectOfType<Camera>(); } 
-    }
+    public CharacterController Controller { get { return controller = controller ?? GetComponent<CharacterController>(); } }
+    public Camera CharacterCamera { get { return characterCamera = characterCamera ?? FindObjectOfType<Camera>(); } }
+    public Animator CharacterAnimator { get { return animator = animator ?? GetComponent<Animator>(); } }
 
     void Update()
     {
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
+        isSprint = Input.GetKey(KeyCode.LeftShift);
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
         Vector3 rotatedMovement = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f) * movement.normalized;
 
-        Controller.Move(rotatedMovement * movementSpeed * Time.deltaTime);
+        float currentSpeed = isSprint ? sprintSpeed : movementSpeed;
+        Controller.Move(rotatedMovement * currentSpeed * Time.deltaTime);
 
         if (rotatedMovement.sqrMagnitude > 0.0f)
         {
             rotationAngle = Mathf.Atan2(rotatedMovement.x, rotatedMovement.z) * Mathf.Rad2Deg;
+            targetAnimationSpeed = isSprint ? 1.0f : 0.5f;
         }
+        else
+        {
+            targetAnimationSpeed = 0.0f;
+        }
+        CharacterAnimator.SetFloat("Speed", Mathf.Lerp(CharacterAnimator.GetFloat("Speed"), targetAnimationSpeed, animationBlendSpeed));
         Quaternion currentRotation = Controller.transform.rotation;
         Quaternion targetRotation = Quaternion.Euler(0.0f, rotationAngle, 0.0f);
         Controller.transform.rotation = Quaternion.Lerp(currentRotation, targetRotation, rotationSpeed);
