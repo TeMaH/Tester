f8f8a1885903aa14f722f8ee223d4735fa031cd6
Artem Mazgo <artsiom.mazgo@gmail.com>
2021-02-16 21:45:25 +0300

    Added Jump

--- Assets/Character.cs
+++ Assets/Character.cs
@@ -8,6 +8,7 @@ public class Character : MonoBehaviour
     public float sprintSpeed = 5.0f;
     public float rotationSpeed = 0.2f;
     public float animationBlendSpeed = 0.2f;
+    public float jumpSpeed = 7.0f;
 
     CharacterController controller;
     Animator animator;
@@ -15,6 +16,10 @@ public class Character : MonoBehaviour
     float rotationAngle = 0.0f;
     float targetAnimationSpeed = 0.0f;
     bool isSprint = false;
+
+    float speedY = 0.0f;
+    float gravity = -9.81f;
+    bool isJumping = false;
 
     public CharacterController Controller { get { return controller = controller ?? GetComponent<CharacterController>(); } }
     public Camera CharacterCamera { get { return characterCamera = characterCamera ?? FindObjectOfType<Camera>(); } }
@@ -25,12 +30,40 @@ public class Character : MonoBehaviour
         float vertical = Input.GetAxis("Vertical");
         float horizontal = Input.GetAxis("Horizontal");
 
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            isJumping = true;
+            CharacterAnimator.SetTrigger("Jump");
+            speedY += jumpSpeed;
+        }
+        if (!Controller.isGrounded)
+        {
+            speedY += gravity * Time.deltaTime;
+        }
+        else if(speedY < 0.0f)
+        {
+            speedY = 0.0f;
+        }
+        CharacterAnimator.SetFloat("SpeedY", speedY / jumpSpeed);
+        if (isJumping && speedY < 0.0f)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, Vector3.down, out hit, 1f, LayerMask.GetMask("Default")))
+            {
+                isJumping = false;
+                CharacterAnimator.SetTrigger("Land");
+            }
+        }
+
+
+
+        // Movement
         isSprint = Input.GetKey(KeyCode.LeftShift);
         Vector3 movement = new Vector3(horizontal, 0.0f, vertical);
         Vector3 rotatedMovement = Quaternion.Euler(0.0f, CharacterCamera.transform.rotation.eulerAngles.y, 0.0f) * movement.normalized;
-
+        Vector3 verticalMovement = Vector3.up * speedY;
         float currentSpeed = isSprint ? sprintSpeed : movementSpeed;
-        Controller.Move(rotatedMovement * currentSpeed * Time.deltaTime);
+        Controller.Move((verticalMovement + rotatedMovement * currentSpeed) * Time.deltaTime);
 
         if (rotatedMovement.sqrMagnitude > 0.0f)
         {
